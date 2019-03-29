1901cb6747fdc13e61884f915a82e70aa4dd19a6
valquero779 <andresserrano2002@hotmail.com>
2019-03-29 13:48:02 -0500

    version 2.0

--- a_power_b.py
+++ a_power_b.py
@@ -1,33 +1,49 @@
+
 def a_power_b(a,b):
-    prod=1
-    for i in range(0,b):
-        prod=prod*a
-        if i==b-1:
-            print(prod)
 
-    return  prod
+        prod=1
 
-a=int(input("Ingresar valor de la base a: "))
-b=int(input("valor de la potencia b: "))
+        for i in range(0,b):
+            prod=prod*a
+            if i>1000:
+                raise Exception("El rango no es admitido  para la potencia")
+        return  prod
 
-a_power_b(a,b)
+
+
+
+
+try:
+    a=int(input("Ingresar valor de la base a: "))
+    b=int(input("valor de la potencia b: "))
+    a_power_b(a, b)
+    r= a_power_b(a, b)
+    print(r)
+
+except Exception:
+    print("error al digitar")
+
 
 
 c=input("quiere voler a intentarlo? y/n: ")
 if c=="y":
-    while c!="n":
-        d = int(input("nueva base a: "))
-        e = int(input("nueva potencia b: "))
-        if d == 0:
-            break
-        
+       try:
+        while c!="n":
+
+            d = int(input("nueva base a: "))
+            e = int(input("nueva potencia b: "))
+            if d == 0:
+                break
 
 
-        a_power_b(d,e)
 
-        if d==0 :
-            print("muchas gracias")
-
+            a_power_b(d,e)
+            d=a_power_b(d,e)
+            print(d)
+            if d==0 :
+                print("muchas gracias")
+       except Exception:
+           print("Error al digitar")
 else:
     if c=="n":
 
