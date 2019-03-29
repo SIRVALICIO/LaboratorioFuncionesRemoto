c41e5f0153df4f09b575b37a611bd96a100f29ab
valquero779 <andresserrano2002@hotmail.com>
2019-03-29 13:05:11 -0500

    mi primera modificacion

--- a_power_b.py
+++ a_power_b.py
@@ -7,7 +7,31 @@ def a_power_b(a,b):
 
     return  prod
 
-a=int(input("Ingresar valor de la base: "))
-b=int(input("valor de la potencia: "))
+a=int(input("Ingresar valor de la base a: "))
+b=int(input("valor de la potencia b: "))
 
 a_power_b(a,b)
+
+
+c=input("quiere voler a intentarlo? y/n: ")
+if c=="y":
+    while c!="n":
+        d = int(input("nueva base a: "))
+        e = int(input("nueva potencia b: "))
+        if d == 0:
+            break
+        
+
+
+        a_power_b(d,e)
+
+        if d==0 :
+            print("muchas gracias")
+
+else:
+    if c=="n":
+
+        print("muchas gracias")
+
+
+
