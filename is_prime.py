9cbd54e91a4367eac3b4a44c841f199aaaa9f814
valquero779 <andresserrano2002@hotmail.com>
2019-03-29 15:02:36 -0500

    Ultima version

--- is_prime.py
+++ is_prime.py
@@ -5,7 +5,9 @@ def is_prime(n):
         cont+=1
         if n % cont == 0:
             primo += 1
+
     return primo
+contP=0
 try:
     n=int(input("Valor a evaluar: "))
     is_prime(n)
@@ -15,5 +17,37 @@ try:
         print("0")
     else:
      print("1")
+     contP+=1
 except Exception:
     print("-1")
+
+c=input("quiere voler a intentarlo? y/n: ")
+if c=="y":
+       try:
+        while c!="n":
+
+            d = int(input("nueva base a: "))
+            if d<=0:
+                raise Exception("-1")
+
+            if d <=0:
+
+                break
+            is_prime(d)
+            s=is_prime(d)
+            if s > 2:
+                print("0")
+            else:
+                print("1")
+                contP+=1
+
+
+       except Exception:
+           print("-1")
+
+else:
+    if c=="n":
+
+        print("muchas gracias")
+
+print("la cantida de primos fueron: ",contP)
